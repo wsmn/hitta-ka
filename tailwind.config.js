76490b6955dd92d989ca7b70de9983fbf06f3e6f
wsmn <arthurwessman@gmail.com>
2019-07-19 21:27:05 +0200

    H-screen height fixed (navbar height removed)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 module.exports = {
   theme: {
     extend: {
-      fontSize: {
-        'text-2xl': '1rem',
-      },
-      colors: {
-        'white': '#1E1E2F',
-      },
       height: {
         'screen': '200px',
       },
       minHeight: {
-        'screen': '70vh',
+        'screen': '-webkit-calc(100% - 55px)',
       },
     },
   },
