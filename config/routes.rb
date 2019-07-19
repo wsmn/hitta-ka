84cef97dea27711987ac89d4f73f82130f25b429
wsmn <arthurwessman@gmail.com>
2019-07-19 17:56:05 +0200

    Landing layout for new password page and file for custom tailwind css

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get("404", controller: :errors, action: :not_found)
   get("422", controller: :errors, action: :unacceptable)
   get("500", controller: :errors, action: :internal_error)
-  resources(:passwords, controller: "clearance/passwords", only: %i[create new])
+  resources(:passwords, controller: :passwords, only: %i[create new])
   resource(:session, controller: :sessions, only: %i[create])
 
   resources(:users, controller: "clearance/users", only: %i[create]) do
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        'text-2xl': '1rem',
+      },
+      colors: {
+        'white': '#1E1E2F',
+      },
+      height: {
+        'screen': '48px',
+      },
+      minHeight: {
+        'screen': '70vh',
+      },
+    },
+  },
+  variants: {},
+  plugins: []
+}
