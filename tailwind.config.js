532e581981946ffb32d7edbd3cbaf1aaf77ab313
wsmn <arthurwessman@gmail.com>
2019-07-20 12:32:52 +0200

    Removed some custom css and fixed height with divs instead.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   theme: {
     extend: {
-      height: {
-        'screen': '200px',
-      },
-      minHeight: {
-        'screen': '-webkit-calc(100% - 55px)',
-      },
     },
   },
   variants: {},
