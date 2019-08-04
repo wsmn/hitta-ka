ec20ac01c7cba5542a4a6d68c569005f5d2d6653
David Wessman <david.wesmn@gmail.com>
2019-08-04 17:58:06 +0200

    Mapbox: working markers!

--- app/javascript/maps/map.js
+++ app/javascript/maps/map.js
@@ -1,3 +1,4 @@
+import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl";
 
 const fitMapToMarkers = (map, features) => {
@@ -9,45 +10,128 @@ const fitMapToMarkers = (map, features) => {
 const handleMapBox = {
   initialize() {
     const mapElement = document.getElementById("map");
+    if (!mapElement) {
+      return;
+    }
     mapboxgl.accessToken = mapElement.dataset.accessToken;
-    const companies = JSON.parse(mapElement.dataset.companies);
-    console.log(companies);
-    const coordinates = companies.features[0].geometry.coordinates;
-    console.log(coordinates);
+
     const map = new mapboxgl.Map({
       container: "map",
-      style: "mapbox://styles/mapbox/streets-v10",
-      center: coordinates,
-      zoom: 11.8
+      style: "mapbox://styles/mapbox/streets-v10"
     });
 
-    map.addLayer({
-      id: "places",
-      type: "symbol",
-      source: {
+    map.on("load", function() {
+      const companies = JSON.parse(mapElement.dataset.companies);
+      map.addSource("companies", {
         type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: [
-            {
-              type: "Feature",
-              properties: {
-                description:
-                  '<strong>Make it Mount Pleasant</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
-                icon: "theatre"
-              },
-              geometry: {
-                type: "Point",
-                coordinates: coordinates,
-              }
-            }
+        data: companies,
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50
+      });
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "companies",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1"
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40
           ]
         }
-      },
-      layout: {
-        "icon-image": "{icon}-15",
-        "icon-allow-overlap": true
-      }
+      });
+
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "companies",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12
+        }
+      });
+
+      map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "companies",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 10,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff"
+        }
+      });
+      map.on("click", "clusters", function(e) {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ["clusters"]
+        });
+        const clusterId = features[0].properties.cluster_id;
+
+        map
+          .getSource("companies")
+          .getClusterExpansionZoom(clusterId, function(err, zoom) {
+            if (err) return;
+
+            map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom: zoom
+            });
+          });
+      });
+
+      map.on("mouseenter", "clusters", function(e) {
+        map.getCanvas().style.cursor = "pointer";
+      });
+
+      map.on("mouseleave", "clusters", function() {
+        map.getCanvas().style.cursor = "";
+      });
+
+      map.on("click", "unclustered-point", function(e) {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ["unclustered-point"]
+        });
+        const infoWindow = features[0].properties.info_window;
+        const coordinates = features[0].geometry.coordinates;
+
+        map.easeTo({
+          center: features[0].geometry.coordinates
+        });
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(infoWindow)
+          .addTo(map);
+      });
+
+      map.on("mouseenter", "unclustered-point", function() {
+        map.getCanvas().style.cursor = "pointer";
+      });
+
+      map.on("mouseleave", "unclustered-point", function() {
+        map.getCanvas().style.cursor = "";
+      });
+
+      fitMapToMarkers(map, companies.features);
     });
   },
   destroy() {}
