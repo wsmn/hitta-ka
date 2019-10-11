dfef379468c7f4514614ff9098c7e18dd6838253
David Wessman <david.wesmn@gmail.com>
2019-10-11 17:32:54 +0200

    Rails 6
    - Ran into some issues with real_favicon, could not include manifest.

--- db/migrate/20190706154353_create_companies.rb
+++ db/migrate/20190706154353_create_companies.rb
@@ -1,11 +1,10 @@
 class CreateCompanies < ActiveRecord::Migration[5.2]
-
   def change
     enable_extension("pgcrypto") unless extension_enabled?("pgcrypto")
     create_table :companies, id: :uuid, default: "gen_random_uuid()" do |t|
       t.string :name, null: false, index: true
       t.string :slug, index: true, unique: true
-      t.jsonb :address, null: false, default: '{}'
+      t.jsonb :address, null: false, default: "{}"
       t.decimal :latitude, index: true
       t.decimal :longitude, index: true
 
