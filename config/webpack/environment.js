d0fd697e4b6a9eeac63b0fa9799a61fd38789ba6
David Wessman <david.wesmn@gmail.com>
2019-10-19 18:34:57 +0200

    Adds tailwind UI navigation
    - Uses Vue!

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
 
 const nodeModulesLoader = environment.loaders.get('nodeModules')
