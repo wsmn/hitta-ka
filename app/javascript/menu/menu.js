da36bfc94ba4de4f078ca2535114db4e6242cf67
wsmn <arthurwessman@gmail.com>
2019-10-21 15:14:25 +0200

    Updates test.

---
+++ app/javascript/menu/menu.js
@@ -0,0 +1,23 @@
+const menuToggle = {
+  addListener(event) {
+    event.preventDefault();
+    const menuNodes = document.getElementsByClassName('navbar-menu');
+    const menu = menuNodes[0];
+    const active = menu.classList.contains('is-active');
+    if (active) {
+      menu.classList.remove('is-active');
+    } else {
+      menu.classList.add('is-active');
+    }
+  },
+  initialize() {
+    const burgerNode = document.getElementsByClassName('navbar-burger')[0];
+    burgerNode.addEventListener('click', this.addListener);
+  },
+  destroy() {
+    const burgerNode = document.getElementsByClassName('navbar-burger')[0];
+    burgerNode.removeEventListener('click', this.addListener);
+  },
+};
+
+export default menuToggle;
--- app/javascript/packs/navigation.js
+++ app/javascript/packs/navigation.js
@@ -1,7 +1,6 @@
 
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
-import App from '../app.vue'
 
 Vue.use(TurbolinksAdapter)
 
