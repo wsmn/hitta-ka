f38c4a843c55e5d4cfc7de16d4800d2197baef2d
David Wessman <david.wesmn@gmail.com>
2019-12-23 22:29:55 +0100

    Fix smtp typo

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@
 ActionMailer::Base.smtp_settings = {
   user_name: ENV.fetch('SENDGRID_USERNAME'),
   password: ENV.fetch('SENDGRID_PASSWORD'),
-  domain: ENV.fetch('APP_HOST')
+  domain: ENV.fetch('APP_HOST'),
   address: "smtp.sendgrid.net",
   port: 465,
   authentication: :plain,
