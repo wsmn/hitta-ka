0ddffff4d402f785f19c5351d2584af8a63ff5e4
David Wessman <david.wesmn@gmail.com>
2019-12-23 22:37:39 +0100

    Fix smtp try 2

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,8 @@
 # Configure SendGrid
 ActionMailer::Base.smtp_settings = {
-  user_name: ENV.fetch('SENDGRID_USERNAME'),
-  password: ENV.fetch('SENDGRID_PASSWORD'),
-  domain: ENV.fetch('APP_HOST'),
+  user_name: ENV.fetch('SENDGRID_USERNAME', ''),
+  password: ENV.fetch('SENDGRID_PASSWORD', ''),
+  domain: ENV.fetch('APP_HOST', ''),
   address: "smtp.sendgrid.net",
   port: 465,
   authentication: :plain,
