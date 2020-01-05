54655d5ccfacf247ca71e076427b5e7da552ad4c
David Wessman <david.wesmn@gmail.com>
2020-01-05 13:21:48 +0100

    Expand CompanyInfo in CompanyList
    - Adds a "Läs mer" button

---
+++ app/javascript/components/company_info.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex flex-grow-1 w-1/2 z-10 bg-white">
+    <div
+      class="w-full p-4 xl:p-10 flex flex-col bg-white"
+    >
+      <div>
+          <button
+          class="absolute top-0 right-0 mt-2 mr-2 p-2 border border-gray-800 rounded bg-white hover:bg-gray-400 hover:border-gray-300 hover:text-white"
+          @click="close"
+        >
+          Stäng
+        </button>
+        <div class="w-full flex mr-2">
+          <div>
+            <div
+              class="bg-red-400 rounded-lg h-16 w-16 xl:h-20 xl:w-20 xxl:h-24 xxl:w-24 flex items-center justify-center text-white font-bold"
+            >Logo</div>
+          </div>
+          <div class="w-11/12 ml-2 xl:ml-4">
+            <p class="font-bold text-xl xxl:text-2xl text-blue-900">{{ company.name }}</p>
+            <p class="text-sm xxl:text-lg text-blue-900 mt-1 font-light">{{ company.address }}</p>
+          </div>
+        </div>
+        <div class="flex-col mt-4 xl:mt-10 xxl:mt-12">
+          <p class="font-semibold text-lg xl:text-xl xxl:text-2xl text-blue-900">About the company</p>
+          <p class="text-sm xxl:text-base text-gray-600 mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed
+            viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat.
+            <a
+              href="#"
+              class="text-blue-400"
+            >Läs mer...</a>
+          </p>
+          <ul class="mt-4 xl:mt-8 xxl:mt-10">
+            <li>
+              <div class="w-5 inline-block text-center text-xl xxl:text-2xl">
+                <i class="fas fa-map-marker-alt text-blue-900 mt-2 fa-2xl"></i>
+              </div>
+              <div class="text-gray-600 inline ml-2 text-sm xxl:text-base">Helsingborg</div>
+            </li>
+            <li class="-mt-1">
+              <div class="w-4 inline-block text-center text-xl xxl:text-2xl">
+                <i class="fas fa-car text-blue-900 mt-2 fa-2xl"></i>
+              </div>
+              <div
+                class="text-gray-600 inline ml-3 text-sm xxl:text-base"
+              >Helsingborg, Höganäs, Klippan, Lund</div>
+            </li>
+            <li class="-mt-1">
+              <div class="w-4 inline-block text-center text-xl xxl:text-2xl">
+                <i class="fas fa-user-tie text-blue-900 mt-2 fa-2xl"></i>
+              </div>
+              <div
+                class="text-gray-600 inline ml-3 text-sm xxl:text-base"
+              >Certifierad kontrollansvarig, Projektledare</div>
+            </li>
+          </ul>
+        </div>
+        <div class="w-full flex mt-6 xl:mt-12">
+          <div class="w-1/2 px-2">
+            <form-modal :company="company"></form-modal>
+          </div>
+          <div class="w-1/2 px-2">
+            <message-modal :company="company"></message-modal>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CompanyInfo",
+  props: ["company"],
+  data: () => ({}),
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
