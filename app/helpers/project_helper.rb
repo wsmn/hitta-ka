d6c88da2e8f8f6022f895305df2b336e13e3ec38
David Wessman <david.wesmn@gmail.com>
2020-03-01 18:11:29 +0100

    Filter projects by status

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -22,6 +22,8 @@ module ProjectHelper
       "bg-blue-400 text-white"
     when "active"
       "bg-green-400 text-gray-900"
+    when "completed"
+      "bg-green-800 text-white"
     when "archived"
       "bg-gray-400 text-white"
     when "paused"
@@ -29,5 +31,25 @@ module ProjectHelper
     when "cancelled"
       "bg-red-400 text-white"
     end
+  end
+
+  def project_status_button(status, current, object: :projects)
+    current ||= :all
+    object ||= :projects
+    css = "border py-2 px-3 rounded mr-2"
+    css = if status == current.to_sym
+      "#{css} bg-blue-600 border-blue-600 hover:bg-blue-500 hover:border-blue-500 text-white"
+    else
+      "#{css} bg-gray-600 border-gray-600 hover:bg-gray-500 hover:border-gray-500 text-gray"
+    end
+    if status == :all
+      path = polymorphic_path(object)
+      text = t("model.project.status.all")
+    else
+      path = polymorphic_path(object, status: status)
+      text = project_status(status)
+    end
+
+    link_to(text, path, class: css)
   end
 end
