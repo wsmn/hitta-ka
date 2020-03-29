25a3bf4fbbdda810bc7fae31a59f01b4a070f85e
David Wessman <david.wesmn@gmail.com>
2020-03-29 11:28:19 +0200

    Search customers and change ordering
    - Solves Turbolinks, Alpinejs and re-rendering partials which includes
      Alpine-code.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,11 +2,15 @@ class Customer < ApplicationRecord
   belongs_to(:organisation)
   has_many(:projects, dependent: :destroy)
   has_many(:tasks, through: :projects)
-  has_many(:invoices)
+  has_many(:invoices, dependent: :destroy)
 
   validates(:name, presence: true)
   store_accessor(:address, :street, :city, :state, :zip_code)
   store_accessor(:contact_information, :email, :phone)
+
+  def self.searchable_columns
+    %i[name address contact_information]
+  end
 
   def to_s
     "(##{id}) #{name}"
