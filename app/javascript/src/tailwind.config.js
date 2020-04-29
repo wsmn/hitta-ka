4af87d8cc75440216e24468a5a0c73ecc39682fd
wsmn <arthurwessman@gmail.com>
2020-04-29 15:51:21 +0200

    Added the font Inter and fixed some pdf rendering issues. (#233)

--- app/javascript/src/tailwind.config.js
+++ app/javascript/src/tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   prefix: "",
   important: false,
   separator: ":",
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      },
+    },
+  },
   variants: {},
   corePlugins: {},
   plugins: [
